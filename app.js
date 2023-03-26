b075a0502f87832642e7b3ccf854dc6444892b44
unknown <rommelmdalisay@gmail.com>
2023-03-26 19:20:12 +0800

    render url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const errorMiddleware = require('./middlewares/errors');
 app.use(express.json({ limit: '100mb' }));
 // app.set("trust proxy", 1);
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://shopit-frontend-fl8j.onrender.com",
     credentials: true}))
 app.use(cookieParser());
 app.use(express.urlencoded({limit: "100mb", extended: true }));
